aa93ec1ec14b87bde213faeab703c2442e51b936
Vadim Usoltsev <vusoltsev@petrosoftinc.com>
2017-05-08 14:23:51 +0300

    - Small Versioning Automation for IDEA

---
+++ version.js
@@ -0,0 +1,12 @@
+var fs = require( "fs" );
+var file = "./FlexCommander-app.xml";
+
+var VERSION_RXP = /<versionNumber>(\d+).(\d+).(\d+)<\/versionNumber>/m;
+var data = fs.readFileSync( file, "utf8" );
+var digits = VERSION_RXP.exec( data );
+var newVersion = digits[ 1 ] + "." + digits[ 2 ] + "." + (Number( digits[ 3 ] ) + 1 );
+
+data = data.replace( VERSION_RXP, "<versionNumber>" + newVersion + "</versionNumber>" );
+fs.writeFileSync( file, data, "utf8" );
+
+console.log( "NEW VERSION IS: " + newVersion );
